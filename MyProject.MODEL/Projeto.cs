1424c198e419b1a51f7ddf2e71cfff13b1ea998d
HenriqueMalisano <132918521+HenriqueMalisano@users.noreply.github.com>
2023-05-11 11:58:21 -0300

    Add files via upload

---
+++ MyProject.MODEL/Projeto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProject.MODEL;
+
+public partial class Projeto
+{
+    public int Id { get; set; }
+
+    public string NomeDoProjeto { get; set; } = null!;
+
+    public string NomeDoGerente { get; set; } = null!;
+
+    public DateTime InicioProjeto { get; set; }
+
+    public DateTime FinalProjeto { get; set; }
+
+    public string ResumoProjeto { get; set; } = null!;
+
+    public string StatusProjeto { get; set; } = null!;
+}
